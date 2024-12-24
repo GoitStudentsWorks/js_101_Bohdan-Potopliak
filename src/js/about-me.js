85194040acc63cd2cb997ab4c8a337956b1b47c1
Kateryna Kolodynska <kolodinkata@gmail.com>
2024-12-24 16:43:19 +0100

    original file about

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,39 +1,48 @@
-    // import Accordion from 'accordion-js';
-    // import 'accordion-js/dist/accordion.min.css';
+    import Accordion from 'accordion-js';
+    import 'accordion-js/dist/accordion.min.css';
 
-    // new Accordion('.accordion-block', {
-    // showMultiple: true,
-    // openOnInit: [0],
-    // });
+    new Accordion('.accordion-block', {
+    showMultiple: true,
+    openOnInit: [0],
+    });
 
-    // window.addEventListener('load', () => {
-    // document.querySelector('.accordion-block').classList.remove('no-animation');
-    // });
+    window.addEventListener('load', () => {
+    document.querySelector('.accordion-block').classList.remove('no-animation');
+    });
 
-    // import Swiper from 'swiper/bundle';
-    // import 'swiper/css/bundle';
+    import Swiper from 'swiper/bundle';
+    import 'swiper/css';
 
-    // const swiper = new Swiper('.skill', {
-    // direction: 'horizontal',
-    // loop: true,
-    // allowSlidePrev: false,
-    // slidesPerView: 2,
+    const swiper = new Swiper('.skill', {
+    direction: 'horizontal',
+    loop: true,
+    allowSlidePrev: false,
+    slidesPerView: 2,
 
-    // breakpoints: {
-    //     768: {
-    //     slidesPerView: 3,
-    //     },
-    //     1440: {
-    //     slidesPerView: 6,
-    //     },
-    // },
-    // keyboard: {
-    //     enabled: true,
-    // },
-    // navigation: {
-    //     nextEl: '.btn-next',
-    // },
-    // mousewheel: {
-    //     enabled: true,
-    // },
-    // });
+    breakpoints: {
+        768: {
+        slidesPerView: 3,
+        },
+        1440: {
+        slidesPerView: 6,
+        },
+    },
+    keyboard: {
+        enabled: true,
+    },
+    navigation: {
+        nextEl: '.btn-next',
+    },
+    mousewheel: {
+        enabled: true,
+    },
+    });
+
+function updateActiveSlide() {
+  const slides = document.querySelectorAll('.swiper-slide');
+  slides.forEach(slide => slide.classList.remove('active-slide'));
+  slides[swiper.activeIndex].classList.add('active-slide');
+}
+updateActiveSlide();
+
+swiper.on('slideChange', updateActiveSlide);
