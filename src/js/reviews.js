7acc97866d8e89bfcbff437f4ca6388ee951960d
Bohdan Potopliak <bohdan.potoplyak12345@gmail.com>
2024-12-25 22:49:57 +0200

    Update reviews.js

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,3 +1,87 @@
+// import iziToast from "izitoast";
+// import "izitoast/dist/css/iziToast.min.css";
+// import axios from "axios";
+// import Swiper from "swiper";
+// import 'swiper/css';
+// import { Navigation, Keyboard } from 'swiper/modules';
+
+// const apiUrl = "https://portfolio-js.b.goit.study/api/reviews";
+// const cardsContainer = document.querySelector('.cards');
+
+// async function fetchData() {
+//     const { data } = await axios(apiUrl)
+//     return data;
+// }
+
+// fetchData()
+//     .then(data => {
+//         cardsContainer.insertAdjacentHTML("beforeend", createCards(data));
+//     })
+//     .catch(error => {
+//         let serverError = error;
+
+//         let observer = new IntersectionObserver((entries, observer) => {
+//             entries.forEach(entry => {
+//                 if (entry.isIntersecting && serverError) {
+//                     showError("Виникли проблеми з отриманням даних із сервера.");
+//                     cardsContainer.insertAdjacentHTML("beforeend", `<p class="not-found"> Not found</p>`)
+//                     serverError = null;
+//                     observer.disconnect();
+//                 }
+//             });
+//         });
+
+//         const reviewsSection = document.getElementById('reviews');
+//         observer.observe(reviewsSection);
+// })
+
+// function createCards(arr) {
+//     return arr.map(({ author, avatar_url, review }) => `
+//     <li class="card swiper-slide">
+//     <img src="${avatar_url}" alt="Author's photo" class="card-img">
+//     <div class="card-content">
+//     <p class="common-headers">${author}</p>
+//     <p class="common-text"> ${review}</p>
+//     </div>
+//     </li>
+//     `).join("");
+// }
+
+// const swiperReviews = new Swiper('.swiper' && '.swiper-reviews', {
+//     modules: [Navigation, Keyboard ],
+//     speed: 500,
+//     keyboard: {
+//         enabled: true,
+//         onlyInViewport: true,
+//     },
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//     centeredSlidesBounds: true,
+//     loop: false,
+//     slidesPerView: 1,
+//     slidesPerGroup: 1,
+//     spaceBetween: 10,
+//     breakpoints: {
+//         768: {
+//             slidesPerView: 2,
+//             spaceBetween: 17.8
+//         },
+//         1440: {
+//             slidesPerView: 4,
+//             spaceBetween: 16,
+//         }
+//     },
+// });
+
+// function showError(message) {
+//     iziToast.error({
+//         title: 'Error',
+//         message: message,
+//     });
+// }
+
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import axios from "axios";
@@ -9,31 +93,32 @@ const apiUrl = "https://portfolio-js.b.goit.study/api/reviews";
 const cardsContainer = document.querySelector('.cards');
 
 async function fetchData() {
-    const { data } = await axios(apiUrl)
-    return data;
+const { data } = await axios(apiUrl);
+return data;
 }
 
 fetchData()
-    .then(data => {
-        cardsContainer.insertAdjacentHTML("beforeend", createCards(data));
-    })
-    .catch(error => {
-        let serverError = error;
-
-        let observer = new IntersectionObserver((entries, observer) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting && serverError) {
-                    showError("Виникли проблеми з отриманням даних із сервера.");
-                    cardsContainer.insertAdjacentHTML("beforeend", `<p class="not-found"> Not found</p>`)
-                    serverError = null;
-                    observer.disconnect();
-                }
-            });
-        });
-
-        const reviewsSection = document.getElementById('reviews');
-        observer.observe(reviewsSection);
+.then(data => {
+    cardsContainer.insertAdjacentHTML("beforeend", createCards(data));
 })
+.catch(error => {
+    let serverError = error;
+
+    let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting && serverError) {
+        showError("Виникли проблеми з отриманням даних із сервера.");
+        // cardsContainer.insertAdjacentHTML("beforeend", <p class="not-found">Not found</p>);
+        cardsContainer.insertAdjacentHTML("beforeend", '<p class="not-found">Not found</p>');
+        serverError = null;
+        observer.disconnect();
+        }
+    });
+    });
+
+    const reviewsSection = document.getElementById('reviews');
+    observer.observe(reviewsSection);
+});
 
 function createCards(arr) {
     return arr.map(({ author, avatar_url, review }) => `
@@ -47,37 +132,37 @@ function createCards(arr) {
     `).join("");
 }
 
-const swiperReviews = new Swiper('.swiper' && '.swiper-reviews', {
-    modules: [Navigation, Keyboard ],
-    speed: 500,
-    keyboard: {
-        enabled: true,
-        onlyInViewport: true,
+const swiperReviews = new Swiper('.swiper-reviews', {
+modules: [Navigation, Keyboard],
+speed: 500,
+keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+},
+navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+},
+centeredSlidesBounds: true,
+loop: false,
+slidesPerView: 1,
+slidesPerGroup: 1,
+spaceBetween: 10,
+breakpoints: {
+    768: {
+    slidesPerView: 2,
+    spaceBetween: 16,
     },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+    1440: {
+    slidesPerView: 4,
+    spaceBetween: 16,
     },
-    centeredSlidesBounds: true,
-    loop: false,
-    slidesPerView: 1,
-    slidesPerGroup: 1,
-    spaceBetween: 10,
-    breakpoints: {
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 16
-        },
-        1440: {
-            slidesPerView: 4,
-            spaceBetween: 16,
-        }
-    },
+},
 });
 
 function showError(message) {
-    iziToast.error({
-        title: 'Error',
-        message: message,
-    });
+iziToast.error({
+    title: 'Error',
+    message: message,
+});
 }
